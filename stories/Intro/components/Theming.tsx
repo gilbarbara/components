2c1791084b6922e9ae3a1c997960b320300c04bc
Gil Barbara <gilbarbara@gmail.com>
2022-05-04 12:24:56 -0300

    Update Intro pages

---
+++ stories/Intro/components/Theming.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import CodeBlock from './CodeBlock';
+
+import { Box, Paragraph } from '../../../src';
+
+function IntroTheming(): JSX.Element {
+  return (
+    <Box>
+      <Paragraph>You can customize the theme in your app.</Paragraph>
+      <Paragraph mt={0}>
+        Check the default{' '}
+        <a
+          href="https://github.com/gilbarbara/components/blob/main/src/modules/theme.ts"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          theme
+        </a>{' '}
+        for more information.
+      </Paragraph>
+      <CodeBlock language="tsx">{`import { ThemeProvider } from '@emotion/react';
+import { mergeTheme } from '@by-intera/components';
+
+const theme = mergeTheme({
+  colors: {
+    primary: '#ff0044',
+  },
+});
+
+function App() {
+  <ThemeProvider theme={theme}>
+    <Root />
+  </ThemeProvider>
+}`}</CodeBlock>
+    </Box>
+  );
+}
+
+export default IntroTheming;
