c5285e9b332f3b19079c6f3a14d14986277b6702
Gil Barbara <gilbarbara@gmail.com>
2022-05-06 21:46:30 -0300

    Refactor Anchor
    - remove default "size"
    - add "hideDecoration" prop

--- stories/Components/Anchor.stories.tsx
+++ stories/Components/Anchor.stories.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { ComponentMeta, Story } from '@storybook/react';
+import { ComponentMeta } from '@storybook/react';
 
-import { Anchor } from '../../src';
-import { AnchorProps } from '../../src/Anchor';
+import { H1 } from '../../src';
+import { Anchor, AnchorProps } from '../../src/Anchor';
 import { hideProps } from '../__helpers__';
 
 export default {
@@ -10,20 +10,32 @@ export default {
   component: Anchor,
   argTypes: {
     ...hideProps(),
-    href: { control: 'text', defaultValue: 'https://github.com/facebook/react' },
+    bold: { defaultValue: false },
     children: { control: 'text', defaultValue: 'Open in GitHub' },
     display: { control: 'text', defaultValue: 'inline-flex' },
-    target: { control: 'text', defaultValue: '_blank' },
+    hideDecoration: { defaultValue: false },
+    href: { control: 'text', defaultValue: 'https://github.com/gilbarbara/components' },
     iconBefore: { defaultValue: 'github' },
-    bold: { defaultValue: false },
-    size: { defaultValue: 'regular' },
-    variant: { control: 'select', defaultValue: 'primary' },
     shade: { control: 'select' },
+    target: { control: 'text', defaultValue: '_blank' },
+    variant: { control: 'select', defaultValue: 'primary' },
   },
 } as ComponentMeta<typeof Anchor>;
 
-const Template: Story<AnchorProps> = props => {
+export function Basic(props: AnchorProps) {
   return <Anchor {...props} />;
-};
+}
 
-export const Basic = Template.bind({});
+export function WithH1(props: AnchorProps) {
+  return (
+    <H1>
+      <Anchor {...props} />
+    </H1>
+  );
+}
+
+WithH1.args = {
+  children: 'Check our docs',
+  iconBefore: undefined,
+};
+WithH1.storyName = 'With an H1 parent';
