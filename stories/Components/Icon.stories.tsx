477cc8c5b8f9718363608647c2c594bdd12bfdd3
Gil Barbara <gilbarbara@gmail.com>
2022-05-12 21:33:31 -0300

    Add "shade" and "variant" to Icon

--- stories/Components/Icon.stories.tsx
+++ stories/Components/Icon.stories.tsx
@@ -3,18 +3,20 @@ import { Icon, IconProps } from 'src/Icon';
 
 import { icons } from 'src/modules/options';
 
-import { hideProps, marginProps } from '../__helpers__';
+import { colorProps, hideProps, marginProps } from '../__helpers__';
 
 export default {
   title: 'Components/Icon',
   component: Icon,
   args: {
     name: 'check-o',
+    shade: 'mid',
     size: 24,
     spin: false,
   },
   argTypes: {
     ...hideProps(),
+    ...colorProps(),
     ...marginProps(),
     name: { control: 'select', options: icons },
     title: { control: 'text' },
