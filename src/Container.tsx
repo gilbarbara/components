8062f70159aabc1088f8e504a00c8a43b973333d
Gil Barbara <gilbarbara@gmail.com>
2022-05-14 13:40:11 -0300

    Upgrade Container

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,54 +1,59 @@
 import { CSSProperties, forwardRef } from 'react';
-import { css } from '@emotion/react';
+import { css, CSSObject } from '@emotion/react';
 import styled from '@emotion/styled';
 import { StringOrNumber } from '@gilbarbara/types';
 
 import { getTheme, px, responsive } from './modules/helpers';
-import {
-  baseStyles,
-  getStyledOptions,
-  marginStyles,
-  paddingStyles,
-  shadowStyles,
-} from './modules/system';
+import { baseStyles, getStyledOptions, marginStyles, paddingStyles } from './modules/system';
 import { WithChildren, WithMargin, WithPadding } from './types';
 
 export interface ContainerProps extends WithChildren, WithMargin, WithPadding {
-  centered?: boolean;
+  /** @default left */
+  align?: 'left' | 'center' | 'right' | 'stretch';
   fullScreen?: boolean;
   fullScreenOffset?: StringOrNumber;
   style?: CSSProperties;
-  verticalAlign?: 'around' | 'between' | 'bottom' | 'center' | 'evenly' | 'top';
+  /** @default left */
+  textAlign?: 'left' | 'center' | 'right';
+  /** @default start */
+  verticalAlign?: 'start' | 'center' | 'end' | 'space-between' | 'space-around' | 'space-evenly';
   verticalPadding?: boolean;
 }
+
+const flexMap = {
+  center: 'center',
+  right: 'flex-end',
+  left: 'flex-start',
+  stretch: 'stretch',
+};
 
 export const StyledContainer = styled(
   'div',
   getStyledOptions(),
 )<ContainerProps>(props => {
-  const { centered, fullScreen, fullScreenOffset, verticalAlign, verticalPadding } = props;
+  const {
+    align = 'left',
+    fullScreen,
+    fullScreenOffset,
+    textAlign,
+    verticalAlign,
+    verticalPadding,
+  } = props;
   const { spacing } = getTheme(props);
-
-  const justifyContentMap = {
-    around: 'space-around',
-    between: 'space-between',
-    bottom: 'flex-end',
-    center: 'center',
-    evenly: 'space-evenly',
-    top: 'flex-start',
+  const styles: CSSObject = {
+    alignItems: flexMap[align],
+    justifyContent: verticalAlign,
+    textAlign,
   };
-  let centeredStyles;
 
-  if (centered || verticalAlign) {
-    centeredStyles = css`
-      align-items: center;
-      display: flex;
-      flex-direction: column;
-    `;
+  if (fullScreen) {
+    styles.minHeight = fullScreenOffset ? `calc(100vh - ${px(fullScreenOffset)})` : '100vh';
   }
 
   return css`
     ${baseStyles(props)};
+    display: flex;
+    flex-direction: column;
     margin-left: auto;
     margin-right: auto;
     ${responsive({
@@ -74,21 +79,19 @@ export const StyledContainer = styled(
     })}
     position: relative;
     width: 100%;
-    ${centeredStyles};
-    ${fullScreen &&
-    css`
-      min-height: ${fullScreenOffset ? `calc(100vh - ${px(fullScreenOffset)})` : '100vh'};
-    `};
-    ${verticalAlign &&
-    css`
-      justify-content: ${justifyContentMap[verticalAlign]};
-    `};
+    ${css(styles)};
+
     ${marginStyles(props)};
     ${paddingStyles(props)};
-    ${shadowStyles(props)};
   `;
 });
 
 export const Container = forwardRef<HTMLDivElement, ContainerProps>((props, ref) => {
   return <StyledContainer ref={ref} data-component-name="Container" {...props} />;
 });
+
+Container.defaultProps = {
+  align: 'stretch',
+  textAlign: 'left',
+  verticalAlign: 'start',
+};
