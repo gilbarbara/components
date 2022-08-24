408e7e77495fe2f8c7192f5ad70c07c17516b66d
Gil Barbara <gilbarbara@gmail.com>
2022-08-24 13:30:52 -0300

    Upgrade NonIdealState
    - add "direction" and "hideIcon" props
    - add "no-results" to type
    - replace "small" prop with "size"

--- src/NonIdealState.tsx
+++ src/NonIdealState.tsx
@@ -5,9 +5,11 @@ import { omit } from '@gilbarbara/helpers';
 import { AnyObject } from '@gilbarbara/types';
 
 import { Box } from './Box';
-import { H2, H3 } from './Headings';
+import { H1, H2, H3 } from './Headings';
 import { Icon } from './Icon';
 import {
+  backgroundStyles,
+  baseStyles,
   borderStyles,
   flexItemStyles,
   getStyledOptions,
@@ -20,10 +22,13 @@ import {
 import { Paragraph } from './Paragraph';
 import {
   ComponentProps,
+  Direction,
   Icons,
+  Sizes,
   StyledProps,
   WithBorder,
   WithChildrenOptional,
+  WithColor,
   WithFlexItem,
   WithLayout,
   WithMargin,
@@ -35,6 +40,7 @@ import {
 export interface NonIdealStateKnownProps
   extends StyledProps,
     WithBorder,
+    WithColor,
     WithChildrenOptional,
     WithFlexItem,
     WithLayout,
@@ -43,22 +49,34 @@ export interface NonIdealStateKnownProps
     WithRadius,
     WithShadow {
   description?: ReactNode;
+  /** @default vertical */
+  direction?: Direction;
+  /** @default false */
+  hideIcon?: boolean;
   icon?: Icons;
-  small?: boolean;
+  /** @default md */
+  size?: Sizes;
   title?: ReactNode;
-  type?: 'error' | 'not-found' | 'offline';
+  /** @default not-found */
+  type?: 'error' | 'no-results' | 'not-found' | 'offline' | null;
 }
 
 export type NonIdealStateProps = ComponentProps<HTMLDivElement, NonIdealStateKnownProps, 'wrap'>;
 
 export const StyledNonIdealState = styled(
-  Box,
+  'div',
   getStyledOptions('type'),
 )<NonIdealStateProps>(props => {
+  const { direction } = props;
+
   return css`
+    ${direction === 'horizontal' ? 'align-items: center;' : ''};
+    display: ${direction === 'horizontal' ? 'flex' : 'block'};
     margin: 0 auto;
-    text-align: center;
+    text-align: ${direction === 'horizontal' ? 'left' : 'center'};
     width: 100%;
+    ${baseStyles(props)};
+    ${backgroundStyles(props, false)};
     ${borderStyles(props)};
     ${flexItemStyles(props)};
     ${layoutStyles(props)};
@@ -70,55 +88,74 @@ export const StyledNonIdealState = styled(
 });
 
 export function NonIdealState(props: NonIdealStateProps) {
-  const { children, description, icon, small, title, type } = props;
+  const { children, description, direction, hideIcon, icon, size = 'md', title, type } = props;
+  const iconSize = {
+    sm: 48,
+    md: 64,
+    lg: 96,
+  };
+  const isVertical = direction === 'vertical';
+  const template: AnyObject = {};
   const output: AnyObject = {};
-  const iconSize = small ? 64 : 96;
 
   switch (type) {
     case 'error': {
-      output.icon = <Icon name="danger" size={iconSize} />;
-      output.title = 'Something went wrong';
-      output.description = 'An unexpected error has occurred. Try reloading the page.';
+      template.icon = <Icon name="danger" size={iconSize[size]} />;
+      template.title = 'Something went wrong';
+      template.description = 'An unexpected error has occurred. Try reloading the page.';
+      break;
+    }
+    case 'no-results': {
+      template.icon = <Icon name="search" size={iconSize[size]} />;
+      template.title = 'No search results';
+      template.description = "Your search didn't match anything.";
       break;
     }
     case 'not-found': {
-      output.icon = <Icon name="smile-none" size={iconSize} />;
-      output.title = 'Page not found';
-      output.description = "We are sorry, but the page you requested doesn't exist.";
+      template.icon = <Icon name="smile-none" size={iconSize[size]} />;
+      template.title = 'Page not found';
+      template.description = "We are sorry, but the page you requested doesn't exist.";
       break;
     }
     case 'offline': {
-      output.icon = <Icon name="arrows-exchange-alt-v" size={iconSize} />;
-      output.title = 'No connection';
-      output.description = 'Please check our internet connection and try again.';
+      template.icon = <Icon name="arrows-exchange-alt-v" size={iconSize[size]} />;
+      template.title = 'No connection';
+      template.description = 'Please check our internet connection and try again.';
       break;
     }
   }
 
-  if (icon) {
-    output.icon = <Icon name={icon} size={iconSize} />;
-  }
-
-  if (output.icon) {
-    output.icon = <Box>{output.icon}</Box>;
-  }
-
-  if (title || output.title) {
-    output.title = small ? (
-      <H3 mb={0} mt={output.icon ? 'sm' : undefined}>
-        {title || output.title}
-      </H3>
-    ) : (
-      <H2 mb={0} mt={output.icon ? 'sm' : undefined}>
-        {title || output.title}
-      </H2>
+  if (!hideIcon && (icon || template.icon)) {
+    output.icon = (
+      <Box align="center" flexBox justify="center">
+        {icon ? <Icon name={icon} size={iconSize[size]} /> : template.icon}
+      </Box>
     );
   }
 
-  if (description || output.description) {
+  if (title !== null && (title || template.title)) {
+    const content = title || template.title;
+
+    switch (size) {
+      case 'sm': {
+        output.title = <H3 mb={0}>{content}</H3>;
+        break;
+      }
+      case 'md': {
+        output.title = <H2 mb={0}>{content}</H2>;
+        break;
+      }
+      default: {
+        output.title = <H1 mb={0}>{content}</H1>;
+        break;
+      }
+    }
+  }
+
+  if (description !== null && (description || template.description)) {
     output.description = (
-      <Paragraph mt={output.title ? 'md' : undefined}>
-        {description || output.description}
+      <Paragraph mt={output.title ? 'xxs' : undefined}>
+        {description || template.description}
       </Paragraph>
     );
   }
@@ -130,20 +167,24 @@ export function NonIdealState(props: NonIdealStateProps) {
   }
 
   return (
-    <StyledNonIdealState data-component-name="NonIdealState" {...omit(props, 'title')}>
+    <StyledNonIdealState data-component-name="NonIdealState" {...omit(props, 'title', 'type')}>
       {output.icon}
-      {output.title}
-      {output.description}
+      <Box ml={!isVertical ? 'xs' : undefined} mt={isVertical && output.icon ? 'sm' : undefined}>
+        {output.title}
+        {output.description}
+      </Box>
       {output.content}
     </StyledNonIdealState>
   );
 }
 
 NonIdealState.defaultProps = {
-  maxWidth: '480px',
+  direction: 'vertical',
+  hideIcon: false,
+  maxWidth: '600px',
   padding: 'md',
   radius: false,
   shadow: false,
-  small: false,
+  size: 'md',
   type: 'not-found',
 };
