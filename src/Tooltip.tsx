44ee51f758e742bf40a8c13d2eb6666cb1531b8f
Gil Barbara <gilbarbara@gmail.com>
2023-02-20 12:17:48 -0300

    Upgrade Tooltip
    - add delay, disabled, duration, easing and eventType props

--- src/Tooltip.tsx
+++ src/Tooltip.tsx
@@ -2,11 +2,19 @@ import { CSSProperties, isValidElement, ReactNode, useEffect, useMemo, useState 
 import innerText from 'react-innertext';
 import { css, useTheme } from '@emotion/react';
 import styled, { CSSObject } from '@emotion/styled';
+import { omit } from '@gilbarbara/helpers';
 import is from 'is-lite';
 
 import { fadeIn } from './modules/animations';
 import { getColorVariant, getTheme, px } from './modules/helpers';
-import { baseStyles, getStyledOptions, radiusStyles, shadowStyles } from './modules/system';
+import { textDefaultOptions } from './modules/options';
+import {
+  baseStyles,
+  getStyledOptions,
+  radiusStyles,
+  shadowStyles,
+  textStyles,
+} from './modules/system';
 import { Text } from './Text';
 import {
   Sizes,
@@ -26,6 +34,26 @@ interface SharedProps {
   position: 'bottom' | 'left' | 'right' | 'top';
   /** Content wrapping */
   wrap?: Sizes;
+  /** @default 100 */
+  zIndex?: number;
+}
+
+interface AnimationProps {
+  /**
+   * The delay before the tooltip is shown in milliseconds.
+   * @default 180
+   */
+  delay: number;
+  /**
+   * The duration of the animation in milliseconds.
+   * default 260
+   */
+  duration: number;
+  /**
+   * The easing function.
+   * @default 'ease-in-out'
+   */
+  easing: string;
 }
 
 interface ArrowProps {
@@ -34,7 +62,7 @@ interface ArrowProps {
    * @default 4
    */
   arrowDistance: number;
-  /** @default 10 */
+  /** @default 8 */
   arrowLength: number;
   /**
    * The margin for the arrow with start/end alignment.
@@ -49,6 +77,7 @@ interface ColorProps {
 
 export interface TooltipProps
   extends Partial<SharedProps>,
+    Partial<AnimationProps>,
     Partial<ArrowProps>,
     WithChildren,
     WithColor,
@@ -56,8 +85,22 @@ export interface TooltipProps
     WithRadius,
     WithShadow,
     WithTextOptions {
+  /**
+   * Optional aria label for the tooltip.
+   * @default innerText of the content
+   */
   ariaLabel?: string;
   content: ReactNode;
+  /**
+   * Disable the tooltip.
+   * @default false
+   */
+  disabled?: boolean;
+  /**
+   * Trigger type.
+   * @default hover
+   */
+  eventType?: 'click' | 'hover';
   style?: CSSProperties;
 }
 
@@ -163,109 +206,130 @@ const StyledArrow = styled.span<SharedProps & ArrowProps & ColorProps>(props => 
 const StyledBody = styled(
   'span',
   getStyledOptions(),
-)<SharedProps & ArrowProps & ColorProps & WithRadius & WithShadow & WithTextSize>(props => {
-  const { align, arrowDistance, arrowLength, bg, color, position, size, wrap } = props;
-  const { spacing } = getTheme(props);
-  const arrowSpacing = arrowLength + arrowDistance;
+)<SharedProps & AnimationProps & ArrowProps & ColorProps & WithRadius & WithShadow & WithTextSize>(
+  props => {
+    const {
+      align,
+      arrowDistance,
+      arrowLength,
+      bg,
+      color,
+      delay,
+      duration,
+      easing,
+      position,
+      size,
+      wrap,
+      zIndex = 100,
+    } = props;
+    const { spacing } = getTheme(props);
+    const arrowSpacing = arrowLength + arrowDistance;
 
-  const styles: CSSObject = {};
+    const styles: CSSObject = {};
 
-  if (align === 'start') {
-    if (['left', 'right'].includes(position)) {
-      styles.top = arrowDistance;
+    if (align === 'start') {
+      if (['left', 'right'].includes(position)) {
+        styles.top = arrowDistance;
+      } else {
+        styles.left = 0;
+      }
+    } else if (align === 'middle') {
+      if (['left', 'right'].includes(position)) {
+        styles.top = '50%';
+        styles.transform = 'translateY(-50%)';
+      } else {
+        styles.left = '50%';
+        styles.transform = 'translateX(-50%)';
+      }
+    } else if (['left', 'right'].includes(position)) {
+      styles.bottom = arrowDistance;
     } else {
-      styles.left = 0;
+      styles.right = arrowDistance;
     }
-  } else if (align === 'middle') {
-    if (['left', 'right'].includes(position)) {
-      styles.top = '50%';
-      styles.transform = 'translateY(-50%)';
-    } else {
-      styles.left = '50%';
-      styles.transform = 'translateX(-50%)';
-    }
-  } else if (['left', 'right'].includes(position)) {
-    styles.bottom = arrowDistance;
-  } else {
-    styles.right = arrowDistance;
-  }
 
-  switch (position) {
-    case 'bottom': {
-      styles.marginTop = arrowSpacing;
-      styles.top = '100%';
+    switch (position) {
+      case 'bottom': {
+        styles.marginTop = arrowSpacing;
+        styles.top = '100%';
 
-      break;
-    }
-    case 'left': {
-      styles.right = '100%';
-      styles.marginRight = arrowSpacing;
+        break;
+      }
+      case 'left': {
+        styles.right = '100%';
+        styles.marginRight = arrowSpacing;
 
-      break;
-    }
-    case 'right': {
-      styles.left = '100%';
-      styles.marginLeft = arrowSpacing;
+        break;
+      }
+      case 'right': {
+        styles.left = '100%';
+        styles.marginLeft = arrowSpacing;
 
-      break;
-    }
-    case 'top': {
-      styles.bottom = '100%';
-      styles.marginBottom = arrowSpacing;
+        break;
+      }
+      case 'top': {
+        styles.bottom = '100%';
+        styles.marginBottom = arrowSpacing;
 
-      break;
+        break;
+      }
     }
-  }
 
-  switch (size) {
-    case 'small':
-    case 'mid': {
-      styles.padding = `${spacing.xxs} ${spacing.xs}`;
-      break;
+    switch (size) {
+      case 'small':
+      case 'mid': {
+        styles.padding = `${spacing.xxs} ${spacing.xs}`;
+        break;
+      }
+      default: {
+        styles.padding = `${spacing.xs} ${spacing.sm}`;
+        break;
+      }
     }
-    default: {
-      styles.padding = `${spacing.xs} ${spacing.sm}`;
-      break;
-    }
-  }
 
-  let width;
+    let width;
 
-  switch (wrap) {
-    case 'sm': {
-      width = '100px';
-      break;
+    switch (wrap) {
+      case 'sm': {
+        width = '100px';
+        break;
+      }
+      case 'md': {
+        width = '200px';
+        break;
+      }
+      case 'lg': {
+        width = '320px';
+        break;
+      }
     }
-    case 'md': {
-      width = '200px';
-      break;
-    }
-    case 'lg': {
-      width = '320px';
-      break;
-    }
-  }
 
-  return css`
-    animation: ${fadeIn} 0.2s forwards;
-    background-color: ${bg};
-    color: ${color};
-    position: absolute;
-    text-align: center;
-    white-space: ${wrap ? 'initial' : 'nowrap'};
-    width: ${width};
-    ${radiusStyles(props)};
-    ${shadowStyles(props, true)};
-    ${styles};
-  `;
-});
+    return css`
+      animation: ${fadeIn} ${duration}ms forwards ${easing};
+      animation-delay: ${delay}ms;
+      background-color: ${bg};
+      color: ${color};
+      opacity: 0;
+      position: absolute;
+      text-align: center;
+      white-space: ${wrap ? 'initial' : 'nowrap'};
+      width: ${width};
+      z-index: ${zIndex};
+      ${radiusStyles(props)};
+      ${shadowStyles(props, true)};
+      ${textStyles(props)};
+      ${styles};
+    `;
+  },
+);
 
 const StyledContent = styled(Text)`
   position: relative;
   z-index: 10;
 `;
 
-function TooltipBody(props: Omit<TooltipProps, 'children' | 'open'> & ArrowProps & ColorProps) {
+function TooltipBody(
+  props: Omit<TooltipProps, 'children' | 'open'> & AnimationProps & ArrowProps & ColorProps,
+) {
   const {
     align = 'middle',
     arrowDistance,
@@ -275,12 +339,14 @@ function TooltipBody(props: Omit<TooltipProps, 'children' | 'open'> & ArrowProps
     bold,
     color,
     content,
+    italic,
     position = 'right',
     radius = 'xxs',
     shadow,
     size = 'mid',
     style,
     wrap,
+    ...rest
   } = props;
 
   return (
@@ -298,11 +364,12 @@ function TooltipBody(props: Omit<TooltipProps, 'children' | 'open'> & ArrowProps
       size={size}
       style={style}
       wrap={wrap}
+      {...rest}
     >
       {isValidElement(content) ? (
         content
       ) : (
-        <StyledContent bold={bold} data-component-name="TooltipContent" size={size}>
+        <StyledContent bold={bold} data-component-name="TooltipContent" italic={italic} size={size}>
           {content}
         </StyledContent>
       )}
@@ -324,10 +391,15 @@ export function Tooltip(props: TooltipProps): JSX.Element {
   const {
     ariaLabel,
     arrowDistance = 4,
-    arrowLength = 10,
+    arrowLength = 8,
     arrowMargin = 4,
     children,
     content,
+    delay = 180,
+    disabled = false,
+    duration = 260,
+    easing = 'ease-in-out',
+    eventType = 'hover',
     open,
     shade,
     variant = 'gray',
@@ -344,14 +416,20 @@ export function Tooltip(props: TooltipProps): JSX.Element {
     setOpen(open || false);
   }, [open]);
 
+  const handleClick = () => {
+    if (eventType === 'click' && !disabled) {
+      setOpen(!isOpen);
+    }
+  };
+
   const handleMouseEnter = () => {
-    if (!is.boolean(open)) {
+    if (eventType === 'hover' && !is.boolean(open) && !disabled) {
       setOpen(true);
     }
   };
 
   const handleMouseLeave = () => {
-    if (!is.boolean(open)) {
+    if (eventType === 'hover' && !is.boolean(open) && !disabled) {
       setOpen(false);
     }
   };
@@ -360,6 +438,7 @@ export function Tooltip(props: TooltipProps): JSX.Element {
     <StyledTooltip
       aria-label={label}
       data-component-name="Tooltip"
+      onClick={handleClick}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       role="tooltip"
@@ -374,6 +453,9 @@ export function Tooltip(props: TooltipProps): JSX.Element {
           arrowMargin={arrowMargin}
           bg={bg}
           color={color}
+          delay={delay}
+          duration={duration}
+          easing={easing}
         />
       )}
     </StyledTooltip>
@@ -381,14 +463,20 @@ export function Tooltip(props: TooltipProps): JSX.Element {
 }
 
 Tooltip.defaultProps = {
+  ...omit(textDefaultOptions, 'size'),
   align: 'middle',
   arrowDistance: 4,
   arrowMargin: 4,
-  arrowLength: 10,
-  bold: false,
+  arrowLength: 8,
+  delay: 180,
+  disabled: false,
+  duration: 260,
+  easing: 'ease-in-out',
+  eventType: 'hover',
   position: 'bottom',
   radius: 'xxs',
   shade: 'dark',
   size: 'mid',
   variant: 'gray',
+  zIndex: 100,
 };
