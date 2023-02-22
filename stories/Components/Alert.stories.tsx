8af51d34dcde58093482316efcda2b03aba399a1
Gil Barbara <gilbarbara@gmail.com>
2023-02-21 21:55:56 -0300

    Upgrade Alert UI
    - add align prop

--- stories/Components/Alert.stories.tsx
+++ stories/Components/Alert.stories.tsx
@@ -2,7 +2,8 @@ import { Meta } from '@storybook/react';
 
 import { Alert, AlertProps } from 'src/Alert';
 
-import { disableControl, hideProps, spacingProps } from '../__helpers__';
+import { Box, Paragraph } from '../../src';
+import { flexBoxProps, hideProps, hideTable, spacingProps } from '../__helpers__';
 
 export default {
   title: 'Components/Alert',
@@ -14,6 +15,7 @@ export default {
   argTypes: {
     ...hideProps(),
     ...spacingProps(),
+    align: flexBoxProps().align,
     children: { control: 'text' },
     type: { control: 'select' },
   },
@@ -23,23 +25,41 @@ export const Basic = {};
 
 export const Types = {
   argTypes: {
-    children: disableControl(),
-    icon: disableControl(),
-    type: disableControl(),
+    align: hideTable(),
+    children: hideTable(),
+    icon: hideTable(),
+    type: hideTable(),
   },
   render: (props: AlertProps) => (
     <>
       {(
         [
-          { text: 'Registration completed!', type: 'success' },
-          { text: 'Please fill in all the fields before proceeding.', type: 'warning' },
-          { text: 'Oops, your information could not be saved.', type: 'error' },
-          { text: 'Please wait, loading...', type: 'info' },
-          { text: 'Nothing too important, just letting you know.', type: 'neutral' },
+          { content: 'Registration completed!', type: 'success' },
+          { content: 'Please fill in all the fields before proceeding.', type: 'warning' },
+          {
+            align: 'start',
+            content: (
+              <Box ml="xs">
+                <Paragraph bold size="large">
+                  Something went wrong!
+                </Paragraph>
+                <Paragraph mt="xxs">Your information could not be saved.</Paragraph>
+              </Box>
+            ),
+            type: 'error',
+          },
+          { content: 'Please wait, loading...', type: 'info' },
+          { content: 'Nothing too important, just letting you know.', type: 'neutral' },
         ] as const
       ).map(d => (
-        <Alert key={d.type} {...props} mb="md" type={d.type}>
-          {d.text}
+        <Alert
+          key={d.type}
+          {...props}
+          align={('align' in d && d.align) || undefined}
+          mb="md"
+          type={d.type}
+        >
+          {d.content}
         </Alert>
       ))}
     </>
