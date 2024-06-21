9987792efafc2e7b9ade50cd8f1677604a115e50
Gil Barbara <gilbarbara@gmail.com>
2024-06-21 09:51:35 -0300

    Upgrade ButtonSplit
    - replace "invert" prop with "bordered"
    - add "buttonProps" prop

--- src/components/ButtonSplit/ButtonSplit.stories.tsx
+++ src/components/ButtonSplit/ButtonSplit.stories.tsx
@@ -1,9 +1,10 @@
-import { MouseEvent, useState } from 'react';
+import { KeyboardEvent, MouseEvent, useState } from 'react';
+import { objectKeys } from '@gilbarbara/helpers';
 import { action } from '@storybook/addon-actions';
 import { Meta, StoryObj } from '@storybook/react';
 import { expect, fn, userEvent, waitFor, within } from '@storybook/test';
 
-import { ButtonUnstyled, Icon, Spacer } from '~';
+import { Anchor, Box, BoxInline, Icon, Paragraph, Spacer } from '~';
 
 import {
   colorProps,
@@ -12,6 +13,7 @@ import {
   hideProps,
   hideStoryFromDocsPage,
 } from '~/stories/__helpers__';
+import { MenuItemProps } from '~/types/props';
 
 import {
   ButtonSplit,
@@ -39,7 +41,8 @@ export default {
     size: { control: 'radio', options: COMPONENT_SIZES },
   },
   parameters: {
-    minHeight: 250,
+    minHeight: 550,
+    justify: 'center',
   },
 } satisfies Meta<typeof ButtonSplit>;
 
@@ -53,21 +56,21 @@ function ButtonSplitWrapper(props: ButtonSplitProps) {
     action('onClick')(id);
   };
 
-  const handleClickClosure = (closeMenu: () => void, name?: string) => {
-    return () => {
-      setLoading(true);
+  const handleClickItem: MenuItemProps['onToggle'] = (event, closeMenu) => {
+    const { name } = event.currentTarget.dataset;
 
-      if (name) {
-        setActionName(name);
-        action('onClick')(name);
-      }
+    setLoading(true);
 
-      setTimeout(() => {
-        setActionName('');
-        setLoading(false);
-        closeMenu();
-      }, 1500);
-    };
+    if (name) {
+      setActionName(name);
+      action('onClick')(name);
+    }
+
+    setTimeout(() => {
+      setActionName('');
+      setLoading(false);
+      closeMenu();
+    }, 1500);
   };
 
   return (
@@ -80,59 +83,113 @@ function ButtonSplitWrapper(props: ButtonSplitProps) {
       onClick={handleClick}
     >
       <ButtonSplitItem disabled>
-        <ButtonUnstyled color="primary">
+        <BoxInline color="primary">
           <Icon mr="xxs" name="plus-o" />
-          Sign up
-        </ButtonUnstyled>
+          Add recipient
+        </BoxInline>
       </ButtonSplitItem>
-      <ButtonSplitItem disableAutoClose>
-        {({ closeMenu }) => (
-          <ButtonUnstyled
-            color="primary"
-            onClick={handleClickClosure(closeMenu, 'Schedule for later')}
-          >
-            <Icon mr="xxs" name="calendar-due" />
-            Schedule for later
-            {loading && actionName === 'Schedule for later' && (
-              <Icon ml="xxs" name="spinner" spin />
-            )}
-          </ButtonUnstyled>
-        )}
+      <ButtonSplitItem data-name="Schedule for later" disableAutoClose onToggle={handleClickItem}>
+        <Icon mr="xxs" name="calendar-due" />
+        Schedule for later
+        {loading && actionName === 'Schedule for later' && <Icon ml="auto" name="spinner" spin />}
       </ButtonSplitItem>
-      <ButtonSplitItem disableAutoClose>
-        {({ closeMenu }) => (
-          <ButtonUnstyled color="primary" onClick={handleClickClosure(closeMenu, 'Save draft')}>
-            <Icon mr="xxs" name="bookmark" />
-            Save draft
-            {loading && actionName === 'Save draft' && <Icon ml="xxs" name="spinner" spin />}
-          </ButtonUnstyled>
-        )}
+      <ButtonSplitItem data-name="Save draft" disableAutoClose onToggle={handleClickItem}>
+        <Icon mr="xxs" name="bookmark" />
+        Save draft
+        {loading && actionName === 'Save draft' && <Icon ml="auto" name="spinner" spin />}
       </ButtonSplitItem>
-      <ButtonSplitItem disableAutoClose>
-        {({ closeMenu }) => (
-          <ButtonUnstyled color="primary" onClick={handleClickClosure(closeMenu, 'Snooze')}>
-            <Icon mr="xxs" name="clock" />
-            Snooze
-            {loading && actionName === 'Snooze' && <Icon ml="xxs" name="spinner" spin />}
-          </ButtonUnstyled>
-        )}
+      <ButtonSplitItem>
+        <Anchor color="black" href="#">
+          <Icon mr="xxs" name="external" />
+          Open in a new window
+        </Anchor>
       </ButtonSplitItem>
       <ButtonSplitSeparator />
-      <ButtonSplitItem color="red">
-        {({ closeMenu }) => (
-          <ButtonUnstyled color="red" onClick={handleClickClosure(closeMenu, 'Delete')}>
-            <Icon mr="xxs" name="trash" />
-            Delete
-            {loading && actionName === 'Delete' && <Icon ml="xxs" name="spinner" spin />}
-          </ButtonUnstyled>
-        )}
+      <ButtonSplitItem bg="red" data-name="Delete" disableAutoClose onToggle={handleClickItem}>
+        <Icon mr="xxs" name="trash" />
+        Delete
+        {loading && actionName === 'Delete' && <Icon ml="auto" name="spinner" spin />}
       </ButtonSplitItem>
     </ButtonSplit>
   );
 }
 
+const descriptionsMap = {
+  merge:
+    'All commits from the source branch are added to the destination branch via a merge commit.',
+  squash:
+    'All commits from the source branch are added to the destination branch as a single commit.',
+  rebase: 'All commits from the source branch are added to the destination branch individually.',
+};
+
+const labelsMap = {
+  merge: 'Create a merge commit',
+  squash: 'Squash and merge',
+  rebase: 'Rebase and merge',
+} as const;
+
+type SelectorKeys = keyof typeof labelsMap;
+
+function ButtonSplitSelector(props: {
+  onClick: MenuItemProps['onToggle'];
+  selected: SelectorKeys;
+  type: string;
+}) {
+  const { onClick, selected, type } = props;
+
+  return (
+    <ButtonSplitItem
+      bg="gray.100"
+      data-title={labelsMap[selected]}
+      data-type={selected}
+      onToggle={onClick}
+      wrap
+    >
+      <Box bg={type === selected ? 'gray.200' : undefined} padding="xs" radius="xs" width={290}>
+        <Paragraph bold>{labelsMap[selected]}</Paragraph>
+        <Paragraph mt="xxs" size="sm">
+          {descriptionsMap[selected]}
+        </Paragraph>
+      </Box>
+    </ButtonSplitItem>
+  );
+}
+
 export const Basic: Story = {
   render: props => <ButtonSplitWrapper {...props} />,
+};
+
+export const Selector: Story = {
+  args: {
+    bg: 'gray.100',
+    buttonProps: {
+      bold: false,
+    },
+    size: 'sm',
+  },
+  render: function Render(props) {
+    const [title, setTitle] = useState<string>(labelsMap.merge);
+    const [type, setType] = useState('merge');
+
+    const handleClickItem = (event: MouseEvent<HTMLElement> | KeyboardEvent<HTMLElement>) => {
+      const { title: dataTitle = '', type: dataType = '' } = event.currentTarget.dataset;
+
+      setTitle(dataTitle);
+      setType(dataType);
+    };
+
+    const handleClick = () => {
+      action('onClick')(type);
+    };
+
+    return (
+      <ButtonSplit {...props} label={title} onClick={handleClick}>
+        {objectKeys(labelsMap).map(key => (
+          <ButtonSplitSelector key={key} onClick={handleClickItem} selected={key} type={type} />
+        ))}
+      </ButtonSplit>
+    );
+  },
 };
 
 export const Sizes: Story = {
