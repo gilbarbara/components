5bcaf41967e7f1514366e7b20bb5c639ae7f7c28
Gil Barbara <gilbarbara@gmail.com>
2024-07-11 18:32:41 -0300

    Add Code, Description and Info components to stories

---
+++ stories/components/Description.tsx
@@ -0,0 +1,27 @@
+import { Box, type Props } from '~';
+
+import { VariantWithTones } from '~/types';
+
+interface DescriptionProps extends Props.BoxProps {
+  borderColor?: VariantWithTones;
+  intro?: boolean;
+}
+
+export default function Description(props: DescriptionProps) {
+  const { borderColor = 'gray.500', intro, maxWidth = 640, ...rest } = props;
+
+  return (
+    <Box
+      border={{ side: 'all', color: borderColor }}
+      data-testid={null}
+      maxWidth={maxWidth}
+      mb={intro ? 'lg' : undefined}
+      mt={intro ? undefined : 'lg'}
+      padding="sm"
+      radius="sm"
+      style={{ lineHeight: 1.4 }}
+      textAlign="center"
+      {...rest}
+    />
+  );
+}
