e03b1ccd73c09df949e119bcb0ddc7c62702a582
Gil Barbara <gilbarbara@gmail.com>
2024-07-14 19:28:59 -0300

    Upgrade Collapse

--- src/components/Collapse/Collapse.stories.tsx
+++ src/components/Collapse/Collapse.stories.tsx
@@ -1,78 +1,151 @@
-import { useState } from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 import { expect, fn, userEvent, within } from '@storybook/test';
 
-import { Button } from '~';
+import { Box, Button, H3, Icon, Paragraph } from '~';
 
 import {
   disableControl,
   hideProps,
   hideStoryFromDocsPage,
-  marginProps,
+  spacingProps,
 } from '~/stories/__helpers__';
+import Code from '~/stories/components/Code';
+import Description from '~/stories/components/Description';
+import Info from '~/stories/components/Info';
 
 import { Collapse, defaultProps } from './Collapse';
 
 type Story = StoryObj<typeof Collapse>;
 
 export default {
-  title: 'Display/Collapse',
+  title: 'Content/Collapse',
   component: Collapse,
   args: {
     ...defaultProps,
     children: `Fugiat exercitation cillum elit dolor aliquip amet minim ea tempor labore occaecat nisi. Et elit nisi velit duis esse in irure sit. Qui sunt elit sunt id cupidatat. Occaecat in exercitation officia deserunt irure sit ea amet labore aliqua. Nulla laboris reprehenderit ipsum cillum sit duis ea nulla enim labore adipisicing aute aliqua.`,
-    title: 'Lorem ipsum dolor sit amet',
   },
   argTypes: {
     ...hideProps(),
-    ...marginProps(),
+    ...spacingProps(),
     children: { control: 'text' },
     maxHeight: { control: 'number' },
   },
   parameters: {
-    align: 'start',
     layout: 'fullscreen',
     minHeight: 300,
     paddingDocs: 'md',
   },
 } satisfies Meta<typeof Collapse>;
 
-export const Basic: Story = {};
+export const Basic: Story = {
+  args: {
+    title: 'Lorem ipsum dolor sit amet',
+  },
+};
+
+export const Expanded: Story = {
+  args: {
+    defaultOpen: true,
+    title: 'Lorem ipsum dolor sit amet',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    title: 'Lorem ipsum dolor sit amet',
+  },
+};
 
 export const LoadMore: Story = {
   args: {
     initialHeight: 20,
-    title: undefined,
+    showBottomToggle: true,
   },
   argTypes: {
-    defaultOpen: disableControl(),
-    hideToggle: disableControl(),
+    showBottomToggle: disableControl(),
     initialHeight: disableControl(),
     title: disableControl(),
   },
-  render: function Render(props) {
-    const [open, setOpen] = useState(false);
-
-    const handleClick = () => {
-      setOpen(!open);
-    };
-
+  render: props => {
     return (
       <>
-        <Collapse mb="md" {...props} open={open} />
-        <Button onClick={handleClick} size="sm">
-          {open ? 'Hide' : 'Load more'}
-        </Button>
+        <Box textAlign="left">
+          <Collapse mb="md" {...props} />
+        </Box>
+        <Description>
+          You can also show a toggle at the bottom of the content to open/close it.
+          <br />
+          <br />
+          Set the <Code>showBottomToggle</Code> prop to <Code>true</Code> and the{' '}
+          <Code>initialHeight</Code> prop to the height you want the content in its collapsed state.
+          <Info icon="info" nested>
+            You can customize the toggle with the <Code>bottomToggle</Code>
+            prop.
+          </Info>
+        </Description>
       </>
     );
   },
 };
 
-export const Open: Story = {
+export const LoadMoreWithTitle: Story = {
+  args: {
+    bottomToggle: ({ isOpen, toggleProps }) => (
+      <Button bg="gray.200" size="xs" {...toggleProps}>
+        {isOpen ? 'Close' : 'Open'}
+      </Button>
+    ),
+    hideHeaderToggle: true,
+    showBottomToggle: true,
+    title: 'Lorem ipsum dolor sit amet',
+  },
+  argTypes: {
+    title: disableControl(),
+  },
+  render: props => {
+    return (
+      <>
+        <Box textAlign="left">
+          <Collapse mb="md" {...props} />
+        </Box>
+        <Description>
+          You can have a title with a bottom toggle by hiding the header toggle with the{' '}
+          <Code>hideHeaderToggle: true</Code> prop.
+          <br />
+          And set a custom component in the <Code>bottomToggle</Code> prop.
+        </Description>
+      </>
+    );
+  },
+};
+
+export const Customized: Story = {
+  args: {
+    bg: 'red.100',
+    color: 'red.700',
+    headerToggle: ({ isOpen }) => (
+      <Icon color={isOpen ? 'red.200' : 'red'} name="chevron-double-left-o" size={28} />
+    ),
+    padding: 'md',
+    radius: 'md',
+    title: (
+      <Box>
+        <H3 mb={0}>The title</H3>
+        <Paragraph color="red.600" mb={0}>
+          The Subtitle
+        </Paragraph>
+      </Box>
+    ),
+  },
+};
+
+export const DefaultOpen: Story = {
   ...hideStoryFromDocsPage(),
   tags: ['hidden'],
   args: {
     defaultOpen: true,
+    title: 'Lorem ipsum dolor sit amet',
   },
 };
 
@@ -87,12 +160,12 @@ export const Tests: Story = {
     const canvas = within(canvasElement);
 
     await canvas.findByTestId('Collapse');
-    await expect(canvas.getByTestId('Collapse')).toHaveAttribute('data-state', 'closed');
+    await expect(canvas.getByTestId('CollapseHeader')).toHaveAttribute('aria-expanded', 'false');
 
     await userEvent.click(canvas.getByTestId('CollapseHeader'));
-    await expect(canvas.getByTestId('Collapse')).toHaveAttribute('data-state', 'open');
+    await expect(canvas.getByTestId('CollapseHeader')).toHaveAttribute('aria-expanded', 'true');
 
     await userEvent.click(canvas.getByTestId('CollapseHeader'));
-    await expect(canvas.getByTestId('Collapse')).toHaveAttribute('data-state', 'closed');
+    await expect(canvas.getByTestId('CollapseHeader')).toHaveAttribute('aria-expanded', 'false');
   },
 };
--- src/components/Collapse/Collapse.tsx
+++ src/components/Collapse/Collapse.tsx
@@ -1,4 +1,13 @@
-import { isValidElement, ReactNode, useEffect, useRef, useState } from 'react';
+import {
+  AriaAttributes,
+  isValidElement,
+  ReactElement,
+  ReactNode,
+  useEffect,
+  useId,
+  useRef,
+  useState,
+} from 'react';
 import { css, keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 import { mergeProps, px } from '@gilbarbara/helpers';
@@ -8,15 +17,45 @@ import is from 'is-lite';
 
 import { useTheme } from '~/hooks/useTheme';
 
+import { getColorTokens } from '~/modules/colors';
 import { getTheme } from '~/modules/helpers';
-import { baseStyles, marginStyles } from '~/modules/system';
+import {
+  baseStyles,
+  colorStyles,
+  marginStyles,
+  outlineStyles,
+  paddingStyles,
+  radiusStyles,
+  shadowStyles,
+} from '~/modules/system';
 
+import { Box } from '~/components/Box';
+import { ButtonUnstyled } from '~/components/ButtonUnstyled';
 import { Icon } from '~/components/Icon';
-import { Paragraph } from '~/components/Paragraph';
+import { Text } from '~/components/Text';
 
-import { WithMargin } from '~/types';
+import {
+  Spacing,
+  StyledProps,
+  WithColors,
+  WithDisabled,
+  WithHTMLAttributes,
+  WithMargin,
+  WithPadding,
+  WithRadius,
+  WithShadow,
+} from '~/types';
 
-export interface CollapseKnownProps extends WithMargin {
+export interface CollapseKnownProps
+  extends StyledProps,
+    AriaAttributes,
+    WithColors,
+    WithDisabled,
+    Omit<WithHTMLAttributes, 'id' | 'title'>,
+    WithMargin,
+    WithPadding,
+    WithRadius,
+    WithShadow {
   /**
    * The duration of the animation when the content is sliding down in seconds.
    * @default 0.3
@@ -27,9 +66,32 @@ export interface CollapseKnownProps extends WithMargin {
    * @default 0.3
    */
   animationExitDuration?: number;
+  /**
+   * The component to display at the bottom if `showBottomToggle` is true.
+   */
+  bottomToggle?: (props: { isOpen: boolean; toggleProps: Record<string, any> }) => ReactElement;
   children: ReactNode;
+  /**
+   * The name of the component.
+   * @default "Collapse"
+   */
+  componentName?: string;
   defaultOpen?: boolean;
-  hideToggle?: boolean;
+  /**
+   * The distance between the header and the toggle
+   *
+   * @default xs
+   */
+  gap?: Spacing;
+  /**
+   * The component to display at the header.
+   */
+  headerToggle?: ReactNode | ((props: { isDisabled: boolean; isOpen: boolean }) => ReactElement);
+  hideHeaderToggle?: boolean;
+  /**
+   * The id of the component.
+   */
+  id?: string;
   /**
    * The height you want the content in its collapsed state.
    * @default 0
@@ -39,47 +101,17 @@ export interface CollapseKnownProps extends WithMargin {
    * The height you want the content in its expanded state.
    */
   maxHeight?: StringOrNumber;
-  onToggle?: (isOpen: boolean) => void;
+  onToggle?: (isOpen: boolean, id?: string) => void;
   open?: boolean;
+  /**
+   * Show a toggle at the bottom of the content to open/close it.
+   * @default false
+   */
+  showBottomToggle?: boolean;
   title?: ReactNode;
 }
 
 export type CollapseProps = Simplify<CollapseKnownProps>;
-
-export const defaultProps = {
-  animationEnterDuration: 0.3,
-  animationExitDuration: 0.3,
-  defaultOpen: false,
-  hideToggle: false,
-  initialHeight: 0,
-} satisfies Omit<CollapseProps, 'children'>;
-
-const getContentAnimation = (initialHeight: StringOrNumber, endHeight: StringOrNumber) => keyframes`
-  0% {
-    height: ${px(initialHeight)};
-    opacity: ${initialHeight ? 1 : 0};
-  }
-  100% {
-    height: ${px(endHeight)};
-    opacity: 1;
-  }
-`;
-
-export const StyledCollapse = styled('div')<Omit<CollapseProps, 'children'>>(props => {
-  return css`
-    ${baseStyles(props)};
-    display: flex;
-    flex-direction: column;
-    ${marginStyles(props)};
-  `;
-});
-
-const Header = styled('header')`
-  align-items: center;
-  cursor: pointer;
-  display: flex;
-  justify-content: space-between;
-`;
 
 interface ContentProps
   extends Pick<
@@ -94,6 +126,74 @@ interface ContentProps
   scrollHeight: number;
   shouldAnimate: boolean;
 }
+
+export const defaultProps = {
+  animationEnterDuration: 0.3,
+  animationExitDuration: 0.3,
+  'aria-label': 'Toggle the content',
+  componentName: 'Collapse',
+  defaultOpen: false,
+  disabled: false,
+  gap: 'xs',
+  headerToggle: <Icon name="chevron-left" title={null} />,
+  hideHeaderToggle: false,
+  initialHeight: 0,
+  showBottomToggle: false,
+} satisfies Omit<CollapseProps, 'children'>;
+
+const StyledCollapse = styled('div')<Omit<CollapseProps, 'children'>>(props => {
+  return css`
+    ${baseStyles(props)};
+    display: flex;
+    flex-direction: column;
+    ${colorStyles(props)};
+    ${marginStyles(props)};
+    ${paddingStyles(props)};
+    ${radiusStyles(props)};
+    ${shadowStyles(props)};
+  `;
+});
+
+const Header = styled.button<
+  SetRequired<Pick<CollapseProps, 'bg' | 'color' | 'disabled' | 'gap'>, 'disabled' | 'gap'>
+>(props => {
+  const { bg = 'primary', color, disabled, gap } = props;
+  const { opacity, spacing, ...theme } = getTheme(props);
+  const { mainColor } = getColorTokens(bg, color, theme);
+
+  return css`
+    align-items: center;
+    all: unset;
+    cursor: pointer;
+    display: flex;
+    gap: ${spacing[gap]};
+    justify-content: space-between;
+    ${outlineStyles(mainColor, props)};
+
+    ${disabled &&
+    css`
+      cursor: not-allowed;
+      opacity: ${opacity.medium};
+    `}
+  `;
+});
+
+const getContentAnimation = (
+  initialHeight: StringOrNumber,
+  endHeight: StringOrNumber,
+  marginTop?: string,
+) => keyframes`
+  0% {
+    height: ${px(initialHeight)};
+    opacity: ${initialHeight ? 1 : 0};
+    margin-top: 0;
+  }
+  100% {
+    height: ${px(endHeight)};
+    opacity: 1;
+    margin-top: ${marginTop};
+  }
+`;
 
 const Content = styled('div')<ContentProps>(props => {
   const {
@@ -110,36 +210,59 @@ const Content = styled('div')<ContentProps>(props => {
 
   let animation;
   let height = initialHeight;
+  let marginTop = hasTitle ? spacing.xs : undefined;
 
   if (shouldAnimate) {
-    animation = isOpen
-      ? css`
-          animation: ${getContentAnimation(initialHeight, maxHeight ?? scrollHeight)}
-            ${animationEnterDuration}s ease-in-out forwards;
-        `
-      : css`
-          animation: ${getContentAnimation(initialHeight, maxHeight ?? scrollHeight)}
-            ${animationExitDuration}s ease-in-out reverse;
-        `;
+    animation = css`
+      animation: ${getContentAnimation(initialHeight, maxHeight ?? scrollHeight, marginTop)}
+        ${isOpen ? animationEnterDuration : animationExitDuration}s ease-in-out forwards
+        ${isOpen ? '' : ' reverse'};
+    `;
   } else {
-    height = isOpen ? maxHeight ?? scrollHeight : initialHeight;
+    height = isOpen ? maxHeight || scrollHeight || 'auto' : initialHeight;
+    marginTop = isOpen ? marginTop : undefined;
   }
 
   return css`
     ${animation};
     height: ${px(height)};
+    margin-top: ${marginTop};
     overflow: hidden;
-    ${hasTitle && `margin-top: ${px(spacing.xs)}`};
+  `;
+});
+
+const HeaderToggle = styled('span')<{ isOpen: boolean }>(props => {
+  const { isOpen } = props;
+
+  return css`
+    align-self: center;
+    display: inline-flex;
+    transform: rotate(${isOpen ? -90 : 0}deg);
+    transition: transform 0.3s;
+    transform-origin: center;
   `;
 });
 
 export function Collapse(props: CollapseProps) {
-  const { children, defaultOpen, hideToggle, onToggle, open, title, ...rest } = mergeProps(
-    defaultProps,
-    props,
-  );
+  const {
+    'aria-label': ariaLabel,
+    bottomToggle,
+    componentName,
+    defaultOpen,
+    disabled,
+    gap,
+    headerToggle,
+    hideHeaderToggle,
+    id,
+    onToggle,
+    open,
+    showBottomToggle,
+    title,
+    ...rest
+  } = mergeProps(defaultProps, props);
+  const contentId = useId();
 
-  const isControlled = typeof open === 'boolean';
+  const isControlled = is.boolean(open);
   const [isOpen, setIsOpen] = useState(isControlled ? open : defaultOpen);
   const [shouldAnimate, setShouldAnimate] = useState(false);
   const contentRef = useRef<HTMLDivElement>(null);
@@ -163,33 +286,74 @@ export function Collapse(props: CollapseProps) {
       setShouldAnimate(true);
     }
 
-    onToggle?.(!isOpen);
+    onToggle?.(isControlled ? open : !isOpen, id);
   };
 
+  const hasTitle = Boolean(title);
+  const toggleProps = {
+    'aria-controls': contentId,
+    'aria-expanded': isOpen,
+    'aria-label': ariaLabel,
+    'data-open': isOpen,
+    'data-disabled': disabled,
+    disabled,
+    onClick: handleClickToggle,
+  };
+
+  const content: Record<string, ReactNode> = {};
+
+  if (!hideHeaderToggle) {
+    content.headerToggle = (
+      <HeaderToggle isOpen={isOpen}>
+        {is.function(headerToggle) ? headerToggle({ isOpen, isDisabled: disabled }) : headerToggle}
+      </HeaderToggle>
+    );
+  }
+
+  if (hasTitle) {
+    content.header = (
+      <Header {...getDataAttributes(`${componentName}Header`)} {...toggleProps} {...rest} gap={gap}>
+        {isValidElement(title) ? (
+          title
+        ) : (
+          <Text {...getDataAttributes(`${componentName}Title`)} flex size="lg">
+            {title}
+          </Text>
+        )}
+        {content.headerToggle}
+      </Header>
+    );
+  }
+
+  if (showBottomToggle) {
+    content.footer = (
+      <Box flexBox mt="xs" {...getDataAttributes(`${componentName}Footer`)}>
+        {bottomToggle ? (
+          bottomToggle({ isOpen, toggleProps })
+        ) : (
+          <ButtonUnstyled color="gray.500" size="xs" {...toggleProps}>
+            {isOpen ? 'hide' : 'load more'}
+          </ButtonUnstyled>
+        )}
+      </Box>
+    );
+  }
+
   return (
-    <StyledCollapse
-      {...getDataAttributes('Collapse')}
-      data-state={isOpen ? 'open' : 'closed'}
-      {...rest}
-    >
-      {Boolean(title) && (
-        <Header {...getDataAttributes('CollapseHeader')} onClick={handleClickToggle}>
-          {isValidElement(title) ? title : <Paragraph>{title}</Paragraph>}
-          {!hideToggle && isOpen ? <Icon name="chevron-up" /> : <Icon name="chevron-down" />}
-        </Header>
-      )}
+    <StyledCollapse {...getDataAttributes(componentName)} {...rest}>
+      {content.header}
       <Content
         ref={contentRef}
-        {...getDataAttributes('CollapseContent')}
-        hasTitle={Boolean(title)}
+        {...getDataAttributes(`${componentName}Content`)}
+        hasTitle={hasTitle}
+        id={contentId}
         isOpen={isOpen}
         onAnimationEnd={handleAnimationEnd}
         scrollHeight={contentRef.current?.scrollHeight ?? 0}
         shouldAnimate={shouldAnimate}
         {...rest}
-      >
-        {children}
-      </Content>
+      />
+      {content.footer}
     </StyledCollapse>
   );
 }
