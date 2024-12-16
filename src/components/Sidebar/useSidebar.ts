130b5a2e3fa5361c9118ca13dca66f14817e27c7
Gil Barbara <gilbarbara@gmail.com>
2024-12-16 00:15:45 -0300

    Add Sidebar

---
+++ src/components/Sidebar/useSidebar.ts
@@ -0,0 +1,96 @@
+import { Simplify } from '@gilbarbara/types';
+
+import { useComponentProps } from '~/hooks/useComponentProps';
+
+import {
+  defaultProps as portalDefaultProps,
+  PortalProps,
+  splitPortalProps,
+} from '~/components/Portal/usePortal';
+
+import {
+  DataAttributes,
+  StyledProps,
+  WithBorder,
+  WithColors,
+  WithDimension,
+  WithDisplay,
+  WithFlexBox,
+  WithHTMLAttributes,
+  WithMargin,
+  WithPadding,
+  WithRadius,
+  WithShadow,
+} from '~/types';
+
+export interface SidebarKnownProps
+  extends StyledProps,
+    DataAttributes,
+    WithBorder,
+    WithColors,
+    WithDimension,
+    WithDisplay,
+    WithFlexBox,
+    WithHTMLAttributes,
+    WithMargin,
+    WithPadding,
+    WithRadius,
+    WithShadow,
+    PortalProps {
+  /**
+   * Add a blur effect to the sidebar.
+   * It doesn't work with the default opacity.
+   * @default true
+   */
+  blurred?: boolean;
+  /**
+   * The radius of the blur effect.
+   * @default 4
+   */
+  blurredRadius?: number;
+  /**
+   * Disable the sidebar animation.
+   * @default false
+   */
+  disableAnimation?: boolean;
+  /**
+   * Show the sidebar.
+   * @default false
+   */
+  isOpen: boolean;
+  /**
+   * The opacity of the sidebar (between 0 and 1).
+   * @default 1
+   */
+  opacity?: number;
+  /**
+   * The origin transform of the sidebar.
+   */
+  origin?: 'top' | 'bottom';
+  /**
+   * The side and horizontal origin of the sidebar.
+   * @default 'left'
+   */
+  side?: 'left' | 'right';
+}
+
+export type SidebarProps = Simplify<SidebarKnownProps>;
+
+export const defaultProps = {
+  ...portalDefaultProps,
+  blurred: false,
+  blurredRadius: 4,
+  disableAnimation: false,
+  isOpen: false,
+  opacity: 1,
+  p: 'md',
+  side: 'left',
+  width: 290,
+} satisfies Omit<SidebarProps, 'children' | 'onDismiss'>;
+
+export function useSidebar(props: SidebarProps) {
+  const { componentProps, getDataAttributes } = useComponentProps(props, defaultProps);
+  const [sidebarProps, portalProps] = splitPortalProps(componentProps);
+
+  return { getDataAttributes, portalProps, sidebarProps };
+}
