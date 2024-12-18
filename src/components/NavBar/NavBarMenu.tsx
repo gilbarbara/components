d941eada1994c732ac6a9c802fe6b7869c8bc36a
Gil Barbara <gilbarbara@gmail.com>
2024-12-17 21:10:17 -0300

    Upgrade NavBar
    - add `placement` and `zIndex` props
    - add `hideBreakpoint` prop to NavBarContent
    - use Sidebar in NavBarMenu
    - remove NavBarBrand, NavBarItem and NavBarMenuItem

--- src/components/NavBar/NavBarMenu.tsx
+++ src/components/NavBar/NavBarMenu.tsx
@@ -1,129 +1,33 @@
-import { useEffect } from 'react';
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
-import { noop, px } from '@gilbarbara/helpers';
-import { useSetState } from '@gilbarbara/hooks';
-import { SetRequired } from '@gilbarbara/types';
-import { opacify } from 'colorizr';
-import disableScroll from 'disable-scroll';
-
 import { useComponentProps } from '~/hooks/useComponentProps';
 
-import { getSlideDownAnimation } from '~/modules/animations';
-import { getColorTokens } from '~/modules/colors';
-import { getStyledOptions } from '~/modules/system';
+import { Sidebar } from '~/components/Sidebar';
 
-import { Portal } from '~/components/Portal';
-
-import { WithTheme } from '~/types';
-
-import { NavBarMenuProps, NavBarProps, useNavBarContext } from './useNavBar';
-
-const StyledNavBarMenu = styled('ul', getStyledOptions())<
-  SetRequired<NavBarProps, 'blurred' | 'blurredOpacity' | 'disableAnimation' | 'height'> &
-    WithTheme & { isMenuOpen: boolean; shouldAnimate: boolean }
->(
-  {
-    display: 'flex',
-    flexDirection: 'column',
-    listStyle: 'none',
-    margin: 0,
-    padding: 0,
-  },
-  props => {
-    const {
-      bg,
-      blurred,
-      blurredOpacity,
-      color,
-      disableAnimation,
-      height,
-      isMenuOpen,
-      shouldAnimate,
-      theme,
-    } = props;
-    const { black, darkColor, darkMode, spacing, white } = theme;
-
-    let animation;
-    const totalHeight = `calc(100vh - ${px(height)})`;
-    let selectedBg = darkMode ? darkColor : white;
-    let selectedColor = darkMode ? white : black;
-
-    if (bg) {
-      const { mainColor, textColor } = getColorTokens(bg, color, theme);
-
-      selectedBg = mainColor;
-      selectedColor = textColor;
-    }
-
-    if (blurred) {
-      selectedBg = opacify(selectedBg, blurredOpacity);
-    }
-
-    if (!disableAnimation && shouldAnimate) {
-      animation = css`
-        animation: ${getSlideDownAnimation(totalHeight)} 0.3s ease-in-out forwards
-          ${isMenuOpen ? '' : ' reverse'};
-      `;
-    }
-
-    return css`
-      ${animation};
-      background-color: ${selectedBg};
-      ${blurred && `backdrop-filter: blur(8px);`};
-      color: ${selectedColor};
-      gap: ${spacing.xs};
-      height: ${totalHeight};
-      overflow-y: auto;
-      padding: ${spacing.md};
-      position: fixed;
-      inset: 0;
-      top: ${px(height)};
-      z-index: 20;
-    `;
-  },
-);
+import { NavBarMenuProps, useNavBarContext } from './useNavBar';
 
 export function NavBarMenu(props: NavBarMenuProps) {
+  const { componentProps, getDataAttributes } = useComponentProps(props);
+  const { onClose, onDismiss, onOpen, ...rest } = componentProps;
+
   const {
-    componentProps: { children, theme },
-    getDataAttributes,
-  } = useComponentProps(props);
-  const {
-    state: { isMenuOpen, onScrollPosition, onToggleMenu, ...rest },
+    state: { disableAnimation, height, isMenuOpen, placement },
+    toggleMenu,
   } = useNavBarContext();
-  const [{ shouldAnimate, shouldRender }, setState] = useSetState({
-    shouldAnimate: isMenuOpen,
-    shouldRender: isMenuOpen,
-  });
 
-  useEffect(() => {
-    if (isMenuOpen) {
-      setState({ shouldRender: true, shouldAnimate: true });
-      disableScroll.on();
-    } else {
-      setState({ shouldAnimate: true });
-      disableScroll.off();
-    }
-  }, [isMenuOpen, setState]);
-
-  const handleAnimationEnd = () => {
-    setState({ shouldRender: isMenuOpen, shouldAnimate: false });
+  const handleDismiss = () => {
+    toggleMenu(false);
   };
 
   return (
-    <Portal disableAnimation hideOverlay isOpen={shouldRender} onDismiss={noop} top={rest.height}>
-      <StyledNavBarMenu
-        {...getDataAttributes('NavBarMenu')}
-        isMenuOpen={isMenuOpen}
-        onAnimationEnd={handleAnimationEnd}
-        shouldAnimate={shouldAnimate}
-        theme={theme}
-        {...rest}
-      >
-        {children}
-      </StyledNavBarMenu>
-    </Portal>
+    <Sidebar
+      {...getDataAttributes('NavBarMenu')}
+      {...rest}
+      animationExitDuration={0.3}
+      bottom={placement === 'bottom' ? height : 0}
+      disableAnimation={disableAnimation}
+      isOpen={isMenuOpen}
+      onDismiss={handleDismiss}
+      top={placement === 'top' ? height : 0}
+    />
   );
 }
 
--- src/components/NavBar/NavBarMenuToggle.tsx
+++ src/components/NavBar/NavBarMenuToggle.tsx
@@ -1,126 +1,24 @@
-import { useId, useMemo } from 'react';
-import styled from '@emotion/styled';
-import { useResponsive } from '@gilbarbara/hooks';
-import is from 'is-lite';
-
 import { useComponentProps } from '~/hooks/useComponentProps';
 
-import { formatBreakpoints } from '~/modules/helpers';
-import { getStyledOptions } from '~/modules/system';
+import { MenuToggle } from '~/components/MenuToggle';
+import { MenuToggleProps } from '~/components/MenuToggle/useMenuToggle';
 
-import { NavBarMenuToggleProps, useNavBarContext } from './useNavBar';
+import { useNavBarContext } from './useNavBar';
 
-const Toggle = styled('span', getStyledOptions())`
-  & input {
-    display: none;
-  }
-
-  & label {
-    cursor: pointer;
-    display: block;
-    height: 16px;
-    position: relative;
-    width: 22px;
-  }
-
-  & span {
-    border-bottom: 2px solid currentcolor;
-    display: block;
-    padding-top: 7px;
-    transition-delay: 0.125s;
-
-    &:before,
-    &:after {
-      border-top: 2px solid currentcolor;
-      content: '';
-      left: 0;
-      position: absolute;
-      right: 0;
-      transform-origin: center;
-      transition-delay: 0s;
-    }
-
-    &:before {
-      top: 0;
-    }
-
-    &:after {
-      bottom: 0;
-    }
-  }
-
-  & span,
-  & span:before,
-  & span:after {
-    transition-duration: 0.25s;
-    transition-property: transform, border-color;
-    transition-timing-function: cubic-bezier(0.5, -0.5, 0.5, 1.5);
-  }
-
-  & input:checked + span {
-    border-color: transparent;
-    transition-delay: 0s;
-
-    &:before,
-    &:after {
-      transition-delay: 0.125s;
-    }
-
-    &:before {
-      transform: translateY(7px) rotate(135deg);
-    }
-
-    &:after {
-      transform: translateY(-7px) rotate(-135deg);
-    }
-  }
-`;
-
-export function NavBarMenuToggle(props: NavBarMenuToggleProps) {
-  const { componentProps, getDataAttributes } = useComponentProps(props, {
-    srOnlyText: 'Toggle Menu',
-  });
-  const { hideBreakpoint, icon, srOnlyText, theme, ...rest } = componentProps;
+export function NavBarMenuToggle(props: Omit<MenuToggleProps, 'isOpen'>) {
+  const { componentProps, getDataAttributes } = useComponentProps(props);
   const {
     state: { isMenuOpen },
     toggleMenu,
   } = useNavBarContext();
-  const id = useId();
-
-  const breakpoints = formatBreakpoints(theme);
-  const { min } = useResponsive(breakpoints);
-
-  const handleChange = () => {
-    toggleMenu(!isMenuOpen);
-  };
-
-  const child = useMemo(() => {
-    if (is.function(icon)) {
-      return icon(isMenuOpen);
-    }
-
-    return icon;
-  }, [icon, isMenuOpen]);
-
-  if (hideBreakpoint && min(hideBreakpoint)) {
-    return null;
-  }
-
-  if (child) {
-    return (
-      <div data-open={isMenuOpen} {...getDataAttributes('NavBarMenuToggle')} {...rest}>
-        {child}
-      </div>
-    );
-  }
 
   return (
-    <Toggle data-open={isMenuOpen} {...getDataAttributes('NavBarMenuToggle')} {...rest}>
-      <label aria-label={srOnlyText} htmlFor={id}>
-        <input checked={isMenuOpen} id={id} onChange={handleChange} type="checkbox" />
-        <span />
-      </label>
-    </Toggle>
+    <MenuToggle
+      {...componentProps}
+      {...getDataAttributes('NavBarMenuToggle')}
+      isOpen={isMenuOpen}
+      onToggle={toggleMenu}
+    />
   );
 }
 
